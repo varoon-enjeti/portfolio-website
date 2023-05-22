14c8a120bb29bda3011465d169db794783ffbf92
Varoon Enjeti <97761722+varoon-enjeti@users.noreply.github.com>
2023-05-21 22:11:48 -0500

    wip updates

--- src/components/wip.jsx
+++ src/components/wip.jsx
@@ -4,7 +4,7 @@ import '../styles/wip.css';
 export default function WIP() {
     return (
         <div className="wip">
-            This Website is a Work in Progress :)
+            This Website is a Work-in-Progress :)
         </div>
     )
 }
