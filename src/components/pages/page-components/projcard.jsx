0dda6ae883a9ef13de35ad7b8785884743588c47
Varoon Enjeti <97761722+varoon-enjeti@users.noreply.github.com>
2023-06-14 17:00:46 -0500

    working on projects

--- src/components/pages/page-components/projcard.jsx
+++ src/components/pages/page-components/projcard.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import "../../../styles/pages-styles/page-component-styles/projcard.css"
 
-export default function ProjCard({name, description, tools, image, link}) {
+export default function ProjCard({name, description, tools, image, link, id}) {
     return (
-        <div className="projcard"></div>
+        <div className="projcard" id={id}></div>
     );
 }
--- src/components/pages/projects.jsx
+++ src/components/pages/projects.jsx
@@ -13,41 +13,50 @@ export default function Project({setProject}) {
             <div className="proj-content">
                 <div className="proj-line">
                     <div className="proj-cards">
-                        <ProjCard
-                            name={"Portfolio Website"}
-                            description={"varoon.xyz"}
-                            tools={["Python", "Artificial Intelligence"]}
-                            image={"../../resources/pfp.png"}
-                            link={"https://github.com/varoon-enjeti/portfolio-website"}
-                        />
-                        <ProjCard
-                            name={"Portfolio Website"}
-                            description={"varoon.xyz"}
-                            tools={["Python", "Artificial Intelligence"]}
-                            image={"../../resources/pfp.png"}
-                            link={"https://github.com/varoon-enjeti/portfolio-website"}
-                        />
-                        <ProjCard
-                            name={"Portfolio Website"}
-                            description={"varoon.xyz"}
-                            tools={["Python", "Artificial Intelligence"]}
-                            image={"../../resources/pfp.png"}
-                            link={"https://github.com/varoon-enjeti/portfolio-website"}
-                        />
-                        <ProjCard
-                            name={"Portfolio Website"}
-                            description={"varoon.xyz"}
-                            tools={["Python", "Artificial Intelligence"]}
-                            image={"../../resources/pfp.png"}
-                            link={"https://github.com/varoon-enjeti/portfolio-website"}
-                        />
-                        <ProjCard
-                            name={"Portfolio Website"}
-                            description={"varoon.xyz"}
-                            tools={["Python", "Artificial Intelligence"]}
-                            image={"../../resources/pfp.png"}
-                            link={"https://github.com/varoon-enjeti/portfolio-website"}
-                        />
+                        <div className="proj-top">
+                            <ProjCard
+                                name={"Portfolio Website"}
+                                description={"varoon.xyz"}
+                                tools={["Python", "Artificial Intelligence"]}
+                                image={"../../resources/pfp.png"}
+                                link={"https://github.com/varoon-enjeti/portfolio-website"}
+                                id={"onep"}
+                            />
+                            <ProjCard
+                                name={"Portfolio Website"}
+                                description={"varoon.xyz"}
+                                tools={["Python", "Artificial Intelligence"]}
+                                image={"../../resources/pfp.png"}
+                                link={"https://github.com/varoon-enjeti/portfolio-website"}
+                                id={"twop"}
+                            />
+                        </div>
+                        <div className="proj-bot">
+                            <ProjCard
+                                name={"Portfolio Website"}
+                                description={"varoon.xyz"}
+                                tools={["Python", "Artificial Intelligence"]}
+                                image={"../../resources/pfp.png"}
+                                link={"https://github.com/varoon-enjeti/portfolio-website"}
+                                id={"threep"}
+                            />
+                            <ProjCard
+                                name={"Portfolio Website"}
+                                description={"varoon.xyz"}
+                                tools={["Python", "Artificial Intelligence"]}
+                                image={"../../resources/pfp.png"}
+                                link={"https://github.com/varoon-enjeti/portfolio-website"}
+                                id={"fourp"}
+                            />
+                            <ProjCard
+                                name={"Portfolio Website"}
+                                description={"varoon.xyz"}
+                                tools={["Python", "Artificial Intelligence"]}
+                                image={"../../resources/pfp.png"}
+                                link={"https://github.com/varoon-enjeti/portfolio-website"}
+                                id={"fivep"}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
