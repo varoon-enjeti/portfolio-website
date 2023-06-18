42bb85af1d86310eb7bf269b4f1d857d90461247
Varoon Enjeti <97761722+varoon-enjeti@users.noreply.github.com>
2023-06-18 00:01:31 -0500

    mobile proj

--- src/components/pages/page-components/projcard.jsx
+++ src/components/pages/page-components/projcard.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import "../../../styles/pages-styles/page-component-styles/projcard.css"
 import ghlogo from "../../../resources/ghlogo.png"
 
 export default function ProjCard({name, description, tools, image, link, id}) {
+    const [windowSize, setWindowSize] = useState([
+        window.innerWidth
+    ]);
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+        setWindowSize([window.innerWidth]);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+        window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
     const bannerImageStyle = {
         width: `100%`,
         height: `71%`,
         background: `url(${image})`,
         backgroundSize: 'cover',
+        backgroundRepeat: 'none',
         borderRadius: "15px 15px 0px 0px",
         borderBottom: "4px solid #2C2C2C",
         backgroundPositionY: -25
@@ -18,14 +36,17 @@ export default function ProjCard({name, description, tools, image, link, id}) {
         height: `71%`,
         background: `url(${image})`,
         backgroundSize: 'cover',
+        backgroundRepeat: 'none',
         borderRadius: "8px 8px 0px 0px",
         borderBottom: "2px solid #2C2C2C",
         backgroundPositionY: -25
     }
 
+    const isMobile = windowSize < 613;
+
     return (
         <div className="projcard" id={id}>
-            <div className="proj-frame" style={bannerImageStyle}></div>
+            {isMobile ? <div className="proj-frame" style={bannerMobile}></div> : <div className="proj-frame" style={bannerImageStyle}></div>}
             <div className="proj-card-lower">
                 <p className="proj-card-name">{name}</p>
                 {(id === "threep") ? (<a href='varoon.xyz' className="proj-card-desc">{description}</a>) : (<p className="proj-card-desc">{description}</p>)}
