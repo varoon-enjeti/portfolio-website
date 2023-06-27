d41b564f02630703c081508a22a62a63c308dd9c
varoon-enjeti <varoon.enjeti@gmail.com>
2023-06-27 10:36:46 -0700

    legit tiny stuff

--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { useState } from "react";
 import '../../styles/pages-styles/contact.css';
 
 export default function Contact({setContact}) {
+    const [status, setStatus] = useState("Let's Connect!");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setStatus("Sending...");
+        const { name, email, message } = e.target.elements;
+        let details = {
+            name: name.value,
+            email: email.value,
+            message: message.value,
+        };
+        let response = await fetch("http://localhost:5000/contact", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(details),
+        });
+        setStatus("Lets's Connect");
+        let result = await response.json();
+        alert(result.status);
+    };
+
     return (
         <div className="subpage">
             <button onClick={() => {setContact(false)}} className="close" id="c-close">
@@ -14,7 +37,7 @@ export default function Contact({setContact}) {
                     <input className='cont-form' type="text" placeholder='Name' />
                     <input className='cont-form' type="text" placeholder='Email' />
                     <textarea className='cont-form' id='message' type="text" placeholder='Message' />
-                    <input className='cont-form' id='cont-sub' type="submit" value="Let's Chat!" onClic={() => {}} />
+                    <button className='cont-form' id='cont-btn'>{status}</button>
                 </form>
             </div>
         </div>
