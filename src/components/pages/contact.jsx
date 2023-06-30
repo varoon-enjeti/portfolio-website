77aa848639812a6f54f5ab941e21cfad50415872
varoon-enjeti <varoon.enjeti@gmail.com>
2023-06-30 15:30:57 -0700

    mobile contact

--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -5,7 +5,7 @@ import '../../styles/pages-styles/contact.css';
 export default function Contact({setContact}) {
     return (
         <div className="subpage">
-            <button onClick={() => {}} className="close" id="c-close">
+            <button onClick={() => {setContact(false)}} className="close" id="c-close">
                 <div className="x"></div>
                 <div className="x" id="right"></div>
             </button>
