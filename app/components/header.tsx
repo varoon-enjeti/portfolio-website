345039d9f1ed329efc32937e75fb09ff4c2e805d
varoon-enjeti <varoon.enjeti@gmail.com>
2023-12-23 19:17:50 -0600

    header for now

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,18 +2,22 @@ import Image from 'next/image'
 
 export default function Header() {
     return (
-        <div className="w-full absolute flex flex-col items-start pl-[7px]">
+        <div className="w-full absolute flex flex-col items-start pl-[7px] gap-[11px]">
             <button className="font-sans font-bold text-[100px] tracking-[-0.04em] text-darkgrey h-[93px] relative">Varoon Enjeti</button>
-            <div className="flex">
+            <div className="flex gap-5">
                 <a href="/" className="">
                     <Image
                         src="/pfp.png"
                         alt='Profile Picture'
                         width={500}
                         height={500}
-                        className='w-[92px]'
+                        className='w-[98px] h-[98px] rounded-[30px] border-darkgrey border-[3px]'
                     />
                 </a>
+                <div className="flex flex-col justify-center gap-3">
+                    <div className="font-sans font-bold text-[43px] tracking-[-0.04em] text-darkgrey">Transit Map</div>
+                    <div className="font-sans font-bold text-[43px] tracking-[-0.04em] text-darkgrey">Portfolio</div>
+                </div>
             </div>
         </div>
     );
