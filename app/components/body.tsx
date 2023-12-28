66e63e9315292f9e80f36d47a6ca927d8608e9ea
varoon-enjeti <varoon.enjeti@gmail.com>
2023-12-28 17:58:59 -0600

    yellowline

--- app/components/body.tsx
+++ app/components/body.tsx
@@ -1,10 +1,12 @@
 import Header from "./header";
 import Redline from "./lines/redline";
+import Yellowline from "./lines/yellowline";
 
 export default function Body() {
     return (
         <div className=" w-[calc(100%-50px)] h-[calc(100%-50px)]">
             <Header />
+            <Yellowline />
             <Redline />
         </div>
     );
---
+++ app/components/lines/yellowline.tsx
@@ -0,0 +1,9 @@
+export default function Yelloline() {
+    return (
+        <div className="relative">
+            <div className="bg-yellow h-6 w-[calc(100%-50px-580px)] absolute top-[369px] left-[282px] tl-tr-br-bl sib"></div>
+            <div className="bg-yellow h-32 w-6 absolute top-[265px] left-[282px] tl-tr-br-bl sil"></div>
+            <div className="bg-yellow h-[370px] w-6 absolute top-[72px] left-[calc(100%-50px-580px+382px)] rotate-45 tl-tr-br-bl sitr"></div>
+        </div>
+    );
+}
