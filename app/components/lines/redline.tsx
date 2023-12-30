394ad81e6d8835c49f13ecedf28c8d8671329560
varoon-enjeti <varoon.enjeti@gmail.com>
2023-12-29 19:29:07 -0600

    stuff

--- app/components/lines/redline.tsx
+++ app/components/lines/redline.tsx
@@ -2,13 +2,12 @@ export default function Redline() {
     return (
         <div className="relative">
             <div className="bg-red h-6 w-[calc(100%-50px-760px)] absolute top-[345px] left-[314px] tl-tr-br-bl sit"></div>
-            <div className="bg-red h-[247px] w-6 absolute top-[122px] left-[calc(100%-50px-760px+290px)] tl-tr-br-bl sir"></div>
             <div className="bg-red h-[310px] w-6 absolute top-[345px] left-[314px] tl-tr-br-bl sir"></div>
-            {/*             
-            <div className="bg-red h-6 w-[calc(100%-40px-100px)] absolute top-[40px] left-[40px] tl-tr-br"></div>
-            <div className="bg-red h-[400px] w-6 absolute top-[40px] left-[calc(100%-100px-24px)] tr-br-bl"></div>
-            <div className="bg-red h-[250px] w-6 absolute left-[40px] top-[40px] tl-tr-bl"></div> 
-            */}
+            <div className="bg-red h-[247px] w-6 absolute top-[122px] left-[calc(100%-50px-760px+290px)] tl-tr-br-bl sir">
+                <button onClick={() => {}} type="button" className="open-btn">
+                    {/* <p className="">Projects</p> */}
+                </button>
+            </div>
         </div>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Varoon Enjeti',
+  title: 'Varoon Enjeti - Portfolio',
   description: 'Varoon Enjeti Portfolio Website',
 }
 
