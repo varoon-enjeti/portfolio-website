d0289e7b04a3ae9a57235bb0ae3f5ceb7daef096
varoon-enjeti <varoon.enjeti@gmail.com>
2024-01-14 18:14:33 -0600

    updates

--- app/components/subpages/projects.tsx
+++ app/components/subpages/projects.tsx
@@ -8,6 +8,7 @@ const Projects: React.FC<redProps> = ({ setProject }) => {
             <button onClick={() => {setProject(false)}} className="absolute w-[40px] h-[40px] bg-red rounded-[13px] ml-[5px] mt-[5px] close">
                 <div className="absolute top-[-0.5px] left-[15px] w-1 h-[35px] bg-darkgrey rotate-45 rounded-full"></div>
                 <div className="absolute top-[-0.5px] left-[15px] w-1 h-[35px] bg-darkgrey rotate-[-45deg] rounded-full"></div>
+                <div className="absolute w-full h-[calc(100%-50px)] top-[50px] flex justify-center items-center text-darkgrey">Coming Soon!</div>
             </button>
         </div>
     );
