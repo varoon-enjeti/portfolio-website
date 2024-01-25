56affede53b11d9e70b8f8b7249f63ad8270b9eb
varoon-enjeti <varoon.enjeti@gmail.com>
2024-01-25 15:53:21 -0600

    added subpage titles and new structure

--- app/components/subpages/contact.tsx
+++ app/components/subpages/contact.tsx
@@ -5,11 +5,16 @@ import { blueProps, SetStateType } from '../types';
 const Contact: React.FC<blueProps> = ({ setContact }) => {
     return (
         <div className="w-full h-[calc(100%-49px)] md:h-[calc(100%-108px)] bg-[#DEDEDE] absolute top-[49px] md:top-[108px] left-0 z-10 border-[3px] md:border-4 border-darkgrey rounded-t-[15px]">
-            <button onClick={() => {setContact(false)}} className="absolute w-[40px] h-[40px] bg-blue rounded-[13px] ml-[5px] mt-[5px] close">
-                <div className="absolute top-[-0.5px] left-[15px] w-1 h-[35px] bg-darkgrey rotate-45 rounded-full"></div>
-                <div className="absolute top-[-0.5px] left-[15px] w-1 h-[35px] bg-darkgrey rotate-[-45deg] rounded-full"></div>
-            </button>
-            <div className="absolute w-full h-[calc(100%-50px)] top-[50px] flex justify-center items-center text-darkgrey">Renovations...</div>
+            <div className="flex h-full w-full">
+                <div className="flex h-10 w-full justify-between mx-[6px] my-[6px]">
+                    <button onClick={() => {setContact(false)}} className="w-[40px] h-[40px] bg-blue rounded-[13px] close">
+                        <div className="relative top-[-1px] left-[15px] w-1 h-[35px] bg-darkgrey rotate-45 rounded-full"></div>
+                        <div className="relative top-[-36px] left-[15px] w-1 h-[35px] bg-darkgrey rotate-[-45deg] rounded-full"></div>
+                    </button>
+                    <div className="font-sans text-blue font-bold track tracking-[-0.04em] text-[45px] subptext">Contact</div>
+                    <div className="absolute w-full h-[calc(100%-50px)] top-[50px] flex justify-center items-center text-darkgrey">Renovations...</div>
+                </div>
+            </div>
         </div>
     );
 }
