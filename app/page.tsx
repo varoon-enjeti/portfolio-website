86a68e84616fc352543c02946f37806f1404574a
varoon-enjeti <varoon.enjeti@gmail.com>
2024-10-14 12:18:43 +0900

    cleaned projects, and preload images (testing)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,49 @@
-import Background from './components/background'
+"use client";
+import { useEffect } from "react";
+import Background from "./components/background";
+
+const imagePaths = [
+	"/bluetooth.webp",
+	"/check.png",
+	"/cpp.png",
+	"/drums.png",
+	"/error.png",
+	"/esp.png",
+	"/ghlogo.png",
+	"/headshot.jpg",
+	"/ind.png",
+	"/java.png",
+	"/link - blue.png",
+	"/link.png",
+	"/linkedin.png",
+	"/mail.png",
+	"/mdp.webp",
+	"/mlt.webp",
+	"/node.png",
+	"/nuai.jpeg",
+	"/pacman.png",
+	"/pfp.png",
+	"/python.png",
+	"/react.png",
+	"/sail.jpg",
+	"/sail.png",
+	"/sent.png",
+	"/sk.png",
+	"/ts.png",
+	"/usa.png",
+];
 
 export default function Home() {
-  return (
-    <main className="text-3xl">
-      <Background />
-    </main>
-  )
+	useEffect(() => {
+		imagePaths.forEach((image) => {
+			const img = new Image();
+			img.src = image;
+		});
+	}, []);
+
+	return (
+		<main className="text-3xl">
+			<Background />
+		</main>
+	);
 }
